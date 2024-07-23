17f74e67fff9def0ffed33ed980aef8ed14d22ab
bxljoy <259500313@qq.com>
2024-07-23 02:25:47 +0200

    product of array except self

---
+++ src/product-except-self.ts
@@ -0,0 +1,32 @@
+function productExceptSelf(nums: number[]): number[] {
+  const prefix: number[] = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (i == 0) {
+      prefix[i] = nums[i];
+    } else {
+      prefix[i] = prefix[i - 1] * nums[i];
+    }
+  }
+
+  const postfix: number[] = [];
+  for (let j = nums.length - 1; j >= 0; j--) {
+    if (j == nums.length - 1) {
+      postfix[j] = nums[j];
+    } else {
+      postfix[j] = nums[j] * postfix[j + 1];
+    }
+  }
+
+  const res: number[] = [];
+  for (let k = 0; k < nums.length; k++) {
+    if (k == 0) {
+      res[k] = postfix[k + 1];
+    } else if (k == nums.length - 1) {
+      res[k] = prefix[k - 1];
+    } else {
+      res[k] = prefix[k - 1] * postfix[k + 1];
+    }
+  }
+
+  return res;
+}
